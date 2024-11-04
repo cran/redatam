db9b38af3079769d0fc97e139577af714ce3072f
Mauricio Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-11-04 20:40:02 +0000

    version 2.0.2

--- R/redatam-package.R
+++
@@ -1,6 +0,0 @@
-#' @useDynLib redatam, .registration = TRUE
-#' @keywords internal
-#' @importFrom data.table as.data.table is.data.table setnames `:=` `.SD`
-#' @importFrom janitor make_clean_names
-#' @importFrom stringi stri_enc_detect
-"_PACKAGE"
